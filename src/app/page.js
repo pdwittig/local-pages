850f9cd0aaf3d4c1355ccae2f053387bbe36073b
Philly <pdwittig@gmail.com>
2023-06-08 12:34:22 -0700

    escape quote

--- src/app/page.js
+++ src/app/page.js
@@ -340,7 +340,7 @@ export default function Page() {
               <h2 className="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
               <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No problem.</p>
               <p className="mt-6 text-lg leading-8 text-gray-300">
-                Don't let the lack of a server hold you back. Deploy your app confidently and securely in
+                Don&#39;t let the lack of a server hold you back. Deploy your app confidently and securely in
                 the cloud, with the flexibility to scale and adapt as your needs change.
               </p>
             </div>
